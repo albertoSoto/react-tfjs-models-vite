b4281f115967c68c706f9b347ca0584c120da857
Alberto Soto Fernandez <albert.soto@gmail.com>
2023-08-08 23:41:39 +0200

    update vite

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,44 @@
 import path from 'path'
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+
+import {nodePolyfills} from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // resolve: {
-  //   alias: {
-  //     process: "process/browser",
-  //     stream: "stream-browserify",
-  //     zlib: "browserify-zlib",
-  //     util: 'util'
-  //   }
-  // },
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/lib/index.jsx'),
-      name: 'React Tensorflow Js Models',
-      fileName: (format) => `react-tfjs-models.${format}.js`
-    },
-    rollupOptions: {
-      // externalize deps that shouldn't be bundled
-      // into your library
-      external: ['react', 'react-dom'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          react: 'React'
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, 'src/lib/index.jsx'),
+            name: 'React Tensorflow Js Models',
+            fileName: (format) => `react-tfjs-models.${format}.js`
+        },
+        rollupOptions: {
+            // externalize deps that shouldn't be bundled
+            // into your library
+            external: ['react', 'react-dom'],
+            output: {
+                // Provide global variables to use in the UMD build
+                // for externalized deps
+                globals: {
+                    react: 'React'
+                }
+            }
         }
-      }
-    }
-  },
-  plugins: [react()]
+    },
+    plugins: [
+        react(),
+        nodePolyfills({
+            // To exclude specific polyfills, add them to this list.
+            exclude: [
+                'fs', // Excludes the polyfill for `fs` and `node:fs`.
+            ],
+            // Whether to polyfill specific globals.
+            globals: {
+                Buffer: true, // can also be 'build', 'dev', or false
+                global: true,
+                process: true,
+            },
+            // Whether to polyfill `node:` protocol imports.
+            protocolImports: true,
+        }),]
 })
