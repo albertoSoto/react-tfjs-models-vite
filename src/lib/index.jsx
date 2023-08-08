3173d69c19c81cc8a9026d414c26219c47b44cd2
Alberto Soto Fernandez <albert.soto@gmail.com>
2023-08-08 20:05:22 +0200

    export connectors

--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -1,2 +1,33 @@
 import HelloWorld from './components/HelloWorld';
-export { HelloWorld };
+import VideoPlayback from './components/VideoPlayback';
+import BlazePose from './components/BlazePose';
+import HandPose from './components/HandPose';
+import Camera from './components/Camera';
+import Mousy from './components/Mousy';
+import FaceMesh from './components/FaceMesh';
+import useModel from "./hooks/useModel";
+import BlazePoseLoader from "./models/BlazePoseLoader";
+import FaceMeshLoader from "./models/FaceMeshLoader";
+import HandPoseLoader from "./models/HandPoseLoader";
+import MoveNetLoader from "./models/MoveNetLoader";
+import {getHeadRotation, quaternionFrom} from "./utils/keypoints";
+import {drawHand, drawPose} from "./utils/handpose";
+
+export {
+    HelloWorld,
+    VideoPlayback,
+    BlazePose,
+    Camera,
+    FaceMesh,
+    Mousy,
+    HandPose,
+    drawHand,
+    drawPose,
+    getHeadRotation,
+    quaternionFrom,
+    MoveNetLoader,
+    HandPoseLoader,
+    FaceMeshLoader,
+    BlazePoseLoader,
+    useModel
+};
